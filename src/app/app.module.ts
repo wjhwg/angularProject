766de59c43d3d945e9561f158488bde50701790b
hwg <1466619197@qq.com>
2018-10-25 22:34:17 +0800

    添加组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { StarComponent } from './star/star.component';
+import { ProductComponent } from './product/product.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    StarComponent,
+    ProductComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
